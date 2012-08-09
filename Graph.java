902c3b377663abcafd3a8d851d6efcd4a96194a1
Alexey Kramarenko <kramar42@gmail.com>
2012-08-09 18:57:41 +0300

    Added comments.

--- Graph.java
+++ Graph.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 
 public class Graph
 {
+	// Contains information about edges
 	int [][] data;
+	// Weights of vertexes
 	int [] weights;
+	// What vertexes were visited
 	boolean [] visited;
+	// Sequence of vertexes
 	ArrayList<Integer> path;
 	
 	public Graph()
@@ -27,18 +31,20 @@ public class Graph
 	
 	public void calculateMinimums(int from, int to)
 	{
+		// All vertexes have starting infinity values of weights
 		for (int i = 0; i < weights.length; ++i)
 		{
 			weights[i] = Dijkstra.INFINITY;
 		}
+		// Starting vertex has zero value
 		weights[from] = 0;
 		
 		int current = from;
-		
 		while (current != to)
 		{
 			for (int i = 0; i < data[current].length; ++i)
 			{
+				// If are connected
 				if (data[current][i] != 0 && !visited[i])
 				{
 					int newWeight = weights[current] + data[current][i];
@@ -67,11 +73,13 @@ public class Graph
 		calculateMinimums(from, to);
 		
 		int prev = to;
+		// Going from last to first
 		while (prev != from)
 		{
 			path.add(prev);
 			for (int i = 0; i < weights.length; ++i)
 			{
+				// Finding previous
 				if (data[i][prev] != 0 && weights[i] + data[i][prev] == weights[prev])
 				{
 					prev = i;
@@ -79,6 +87,8 @@ public class Graph
 			}
 		}
 		path.add(from);
+		
+		// Reversing order of vertexes & casting to array of ints
 		Integer[] integerPath = path.toArray(new Integer[path.size()]);
 		int [] returnPath = new int [path.size()];
 		for (int i = 0; i < returnPath.length; ++i)
